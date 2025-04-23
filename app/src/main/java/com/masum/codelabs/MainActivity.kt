f44d10ea1eb544d8e26765a4d46c0da8c655064e
Masum <hydrapikachu29@gmail.com>
2025-04-23 21:01:59 +0530

    Adding text element

--- app/src/main/java/com/masum/codelabs/MainActivity.kt
+++ app/src/main/java/com/masum/codelabs/MainActivity.kt
@@ -4,13 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.masum.codelabs.ui.theme.CodelabsTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +20,35 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CodelabsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                GoodLuck(
+                    message = "Good Luck Saheli for your future",
+                    from  = "From Masum",
+                )
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun GoodLuck (message: String, from: String, modifier: Modifier = Modifier) {
+    Row {
+        Text(
+            text = message,
+            fontSize = 80.sp,
+            lineHeight = 30.sp,
+            modifier = modifier.padding(67.dp),
+        )
+        Text(
+            text = from,
+            fontSize = 50.sp,
+            lineHeight = 30.sp
+        )
+    }
 }
-
-@Preview(showBackground = true)
+@Preview (showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun GoodLuckPreview() {
     CodelabsTheme {
-        Greeting("Android")
+        GoodLuck(message = "Good Luck  Saheli for your future", from  = "From Masum")
     }
 }
