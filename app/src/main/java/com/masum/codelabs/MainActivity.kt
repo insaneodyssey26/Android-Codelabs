f09a524c0fc5f14d9cc61abb259df92b090190b9
Masum <hydrapikachu29@gmail.com>
2025-04-25 22:52:06 +0530

    :

--- app/src/main/java/com/masum/codelabs/MainActivity.kt
+++ app/src/main/java/com/masum/codelabs/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -13,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -41,18 +43,24 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GoodLuck (message: String, from: String, modifier: Modifier = Modifier) {
-    Column {
+    Column{
+        val image = painterResource(R.drawable.usghibli)
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
         Text(
             text = message,
             fontSize = 60.sp,
             lineHeight = 100.sp,
-            modifier = modifier.padding(67.dp),
+            modifier = Modifier.padding(67.dp),
             textAlign = TextAlign.Center,
         )
         Text(
             text = from,
             fontSize = 30.sp,
-            modifier = modifier.padding(20.dp)
+            modifier = Modifier.padding(20.dp)
                 .align(Alignment.End),
         )
     }
@@ -61,10 +69,19 @@ fun GoodLuck (message: String, from: String, modifier: Modifier = Modifier) {
 @Composable
 fun GoodLuckPreview() {
     CodelabsTheme {
-        GoodLuck(message = "Good Luck Saheli for your future", from = "From Masum")
+        GoodLuckImage(
+            message = "Good Luck Saheli for your future",
+            from = "From Masum"
+        )
     }
 }
 
 @Composable
 fun GoodLuckImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.usghibli)
+    Image(
+        modifier = modifier.fillMaxSize(),
+        painter = image,
+        contentDescription = null,
+    )
 }
