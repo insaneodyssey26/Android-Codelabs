b41af4449eba11fe3cdbf2694c1c08acb659736f
Masum <hydrapikachu29@gmail.com>
2025-04-28 22:04:54 +0530

    Launch hoise :)

--- app/src/main/java/com/masum/codelabs/MainActivity.kt
+++ app/src/main/java/com/masum/codelabs/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GoodLuck(
+                    GoodLuckImage(
                         message = "Good Luck Saheli for your future",
                         from = "From Masum",
                     )
