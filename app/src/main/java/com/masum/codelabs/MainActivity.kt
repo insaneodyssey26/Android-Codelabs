4151a26f0677dcb055764cba7821bbbacb25d705
Masum <hydrapikachu29@gmail.com>
2025-05-10 21:09:45 +0530

    Used string resource instead of hardcoded string in message text

--- app/src/main/java/com/masum/codelabs/MainActivity.kt
+++ app/src/main/java/com/masum/codelabs/MainActivity.kt
@@ -17,6 +17,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -34,7 +35,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     GoodLuckImage(
-                        message = "Good Luck Saheli for your future",
+                        message = stringResource(R.string.good_luck_text),
                         from = "God, hope you fulfil our dreams",
                     )
                 }
@@ -72,8 +73,10 @@ fun GoodLuck (message: String, from: String) {
             text = from,
             fontSize = 15.sp,
             fontWeight = FontWeight.SemiBold,
-            modifier = Modifier.padding(20.dp)
-                .align(Alignment.BottomCenter)
+            modifier = Modifier
+                .padding(20.dp)
+                //  .align(Alignment.BottomCenter)
+                .align(alignment = Alignment.BottomCenter)
             )
     }
 }
