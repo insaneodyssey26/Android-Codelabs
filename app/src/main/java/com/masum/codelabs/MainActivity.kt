6a5aa9ca69a0c29bbb809c3b9ede1baa5ae89d4a
Masum <hydrapikachu29@gmail.com>
2025-05-12 12:29:03 +0530

    Updated with latest release build

--- app/src/main/java/com/masum/codelabs/MainActivity.kt
+++ app/src/main/java/com/masum/codelabs/MainActivity.kt
@@ -19,6 +19,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -54,6 +55,7 @@ fun GoodLuck (message: String, from: String) {
             Text(
                 text = message,
                 fontSize = 50.sp,
+                textAlign = TextAlign.Center,
                 fontWeight = FontWeight.SemiBold,
                 lineHeight = 100.sp,
                 modifier = Modifier
@@ -75,7 +77,6 @@ fun GoodLuck (message: String, from: String) {
             fontWeight = FontWeight.SemiBold,
             modifier = Modifier
                 .padding(20.dp)
-                //  .align(Alignment.BottomCenter)
                 .align(alignment = Alignment.BottomCenter)
             )
     }
